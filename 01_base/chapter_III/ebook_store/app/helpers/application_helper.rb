9068a4f94b45dd98e63c1fd9f0ce0ef3bbed3112
lmoreira-runtime <165663989+lmoreira-runtime@users.noreply.github.com>
2024-08-05 17:06:50 +0100

    Changes to be committed: 	modified:   01_base/chapter_III/ebook_store/app/helpers/application_helper.rb 	modified:   01_base/chapter_III/ebook_store/app/views/ebooks/_form.html.erb

--- 01_base/chapter_III/ebook_store/app/helpers/application_helper.rb
+++ 01_base/chapter_III/ebook_store/app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # Helper methods for the application.
 module ApplicationHelper
+  def text_with_label(form, field, options = {})
+    label_text = options.delete(:label) || field.to_s.humanize
+    field_type = options.delete(:field_type) || :text_field
+    additional_classes = options.delete(:class) || 'form-control'
+    label_classes = options.delete(:label_class) || 'control-label'
+
+    content_tag :div, class: 'field form-group mb-3' do
+      concat form.label(field, label_text, class: label_classes)
+      concat form.send(field_type, field, options.merge(class: additional_classes))
+    end
+  end
 end
